2cc2353a1d1d361b15133e4cb5ce27aca652ca55
JeonghoonKim91 <automobile@korea.ac.kr>
2020-08-13 16:04:25 +0900

    annotation modi

--- 1.Utils/customSpatialCrossEntropy.m
+++ 1.Utils/customSpatialCrossEntropy.m
@@ -1,7 +1,8 @@
 function loss = customSpatialCrossEntropy( Y, T )
 
 numObservations = size(Y, 4) * size(Y, 1) * size(Y, 2);
-loss_i = T .* log(customBoundAwayFromZero(double(Y)));
+loss_i = T .* log(customBoundAwayFromZero(Y));
 loss = -sum( sum( sum( sum(double(loss_i), 3).*(1./numObservations), 1), 2));  
 
 end
+
